f3f82252f4daaf303b539c104e1ca217ffcf8f54
Roshan Poudel <poudelera17@gmail.com>
2024-03-14 13:13:55 +0545

    fix: background svg

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,4 @@
-import LoginBackground from "@neo/assets/images/Login.png";
+import LoginBackground from "@neo/assets/images/Login.svg";
 import LoginBanner from "@neo/assets/images/LoginBanner.png";
 import UkFlag from "@neo/assets/images/englishLang.png";
 import Loader from "@neo/assets/images/loader.gif";
