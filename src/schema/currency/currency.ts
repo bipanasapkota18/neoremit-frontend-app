47dac4096592ed9335501b55c5789ace4af73773
Roshan Poudel <roshanbca19@oic.edu.np>
2024-05-22 18:12:43 +0545

    fix: currency validation message

--- src/schema/currency/currency.ts
+++ src/schema/currency/currency.ts
@@ -3,7 +3,8 @@ import * as yup from "yup";
 const currencySchema = yup.object().shape({
   name: yup.string().required("Please enter currency Name"),
   shortName: yup.string().required("Please enter currency Short Name"),
-  Symbol: yup.string().required("Please enter currency Symbol")
+  Symbol: yup.string().required("Please enter currency Symbol"),
+  code: yup.string().required("Please enter currency Code")
 });
 
 export default currencySchema;
